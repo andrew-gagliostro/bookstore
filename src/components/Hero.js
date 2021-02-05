f537a97e6bd9e63aa6a22ea0cc633793d5a4101f
andrew-gagliostro <argagliostro@gmail.com>
2021-02-05 17:27:26 -0500

    changed layout

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const Hero = () => {
     return (
         <section className="hero">
-            <h2>Wisdom Books</h2>
-            <h3>A room without books is like a <br />body without a soul</h3>
+            <h2>Drew's Reads</h2>
+            <h3>I dont really like reading <br />but I know other people do</h3>
             <Link className="btn" to="/books">View All Books</Link>
         </section>
     )
